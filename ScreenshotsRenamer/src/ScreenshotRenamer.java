992daa96663a47e156f19824d2e069bf820c354c
it2051229 <it2051229@yahoo.com>
2016-01-15 10:56:59 +0800

    Initial Code
    Initial working code.

---
+++ ScreenshotsRenamer/src/ScreenshotRenamer.java
@@ -0,0 +1,26 @@
+
+import java.io.File;
+
+public class ScreenshotRenamer {
+
+    // Entry point of the program
+    public static void main(String[] args) throws Exception {
+        File currentDirectory = new File("./");
+        int nextScreenShotNumber = 1;
+
+        for (File file : currentDirectory.listFiles()) {
+            if (file.isFile()) {
+                String filename = file.getName().toLowerCase();
+
+                // Entertain only PNG files
+                if (filename.endsWith(".png")) {
+                    // Ignore figures
+                    if (!filename.contains("figure")) {
+                        file.renameTo(new File("screenshot" + nextScreenShotNumber + ".png"));
+                        nextScreenShotNumber++;
+                    }
+                }
+            }
+        }
+    }
+}
